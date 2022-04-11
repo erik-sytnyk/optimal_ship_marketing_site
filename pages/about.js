689fe3c7b4956da087ea512814368b9683ba3576
Erik Sytnyk <erik.sytnyk@gmail.com>
2022-04-11 18:33:06 +0530

    list preview for about page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
 import Head from 'next/head';
+import {createClient} from '../prismicio';
+import Image from 'next/image';
 
 import Layout, {siteTitle} from '../components/layout';
 
-function About() {
+function About(props) {
+  const data = props?.page?.data;
+  console.log(data);
+
   return (
     <Layout>
       <Head>
@@ -10,8 +15,35 @@ function About() {
       </Head>
 
       <h2>About Page</h2>
+
+      {data['about-item'].map((item, index) => {
+        const image = item.image;
+
+        return (
+          <div key={index}>
+            <h3>{item.title[0].text}</h3>
+            <p>{item.description[0].text}</p>
+            <Image
+              src={image?.url}
+              alt={image?.alt}
+              width={image?.dimensions?.width}
+              height={image?.dimensions?.height}
+            />
+          </div>
+        );
+      })}
     </Layout>
   );
 }
 
 export default About;
+
+export async function getStaticProps({previewData}) {
+  const client = createClient({previewData});
+
+  const page = await client.getSingle('about');
+
+  return {
+    props: {page} // Will be passed to the page component as props
+  };
+}
