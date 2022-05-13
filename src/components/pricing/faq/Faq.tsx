8651bf914a0ac53ddfba41cb57cc63b1472e71a7
Erik Sytnyk <erik.sytnyk@gmail.com>
2022-05-13 13:41:07 +0300

    Pricing Page: "FQA" section

--- src/components/pricing/faq/Faq.tsx
+++ src/components/pricing/faq/Faq.tsx
@@ -7,7 +7,22 @@ import * as styled from './styled';
 function FAQ() {
   const data = dataService.getFAQ();
 
-  return <styled.wrapper></styled.wrapper>;
+  return (
+    <styled.wrapper>
+      <styled.title>{data.title}</styled.title>
+
+      <styled.questionsWrapper>
+        {data.questions.map((item, index) => {
+          return (
+            <styled.container key={index}>
+              <styled.question>{item.question}</styled.question>
+              <styled.answer>{item.answer}</styled.answer>
+            </styled.container>
+          );
+        })}
+      </styled.questionsWrapper>
+    </styled.wrapper>
+  );
 }
 
 export default FAQ;
--- src/components/pricing/faq/styled.ts
+++ src/components/pricing/faq/styled.ts
@@ -1,8 +1,42 @@
 import styled from 'styled-components';
 
-import {mediaQueries} from 'src/styles/shared';
+import {mediaQueries, sectionTitle_3, body_01} from 'src/styles/shared';
 
 export const wrapper = styled.div`
+  padding: 3.6rem 1.6rem 0;
+
   @media ${mediaQueries.tablet} {
+    padding: 3.6rem 13.5rem 0;
   }
+`;
+
+export const title = styled.div`
+  ${sectionTitle_3};
+  margin-bottom: 4rem;
+
+  @media ${mediaQueries.tablet} {
+    white-space: pre-line;
+  }
+`;
+
+export const questionsWrapper = styled.div`
+  @media ${mediaQueries.tablet} {
+    columns: 57.7rem;
+    column-gap: 1.5rem;
+  }
+`;
+
+export const container = styled.div`
+  margin-bottom: 6rem;
+`;
+
+export const question = styled.div`
+  ${body_01};
+  font-weight: 700;
+  margin-bottom: 4rem;
+`;
+
+export const answer = styled.div`
+  white-space: break-spaces;
+  ${body_01};
 `;
