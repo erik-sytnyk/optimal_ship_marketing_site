aacd66781965dd55429f00a4027aebb64d0c3455
Erik Sytnyk <erik.sytnyk@gmail.com>
2022-07-18 08:46:19 +0300

    Revert "ML-2 Marketing site should track in Heap (#8)"
    This reverts commit 7099cb24d337815581d950de9c56cb8c460a8f04.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,7 @@
 import '../styles/index.scss';
-import Script from 'next/script';
-import env from "@beam-australia/react-env";
 
 function MyApp({Component, pageProps}) {
-  return (
-  <>
-    <Script src="/__ENV.js" strategy="afterInteractive" />
-
-    {env("HEAP_ID") && <Script id="analytics" strategy="afterInteractive">
-      {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-      heap.load(${env("HEAP_ID")});`}
-    </Script>}
-
-   <Component {...pageProps} />;
-  </>
-  )
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
