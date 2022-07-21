a980383b60ee812ff59aa52c246e7da58cdf6de4
Erik Sytnyk <erik.sytnyk@gmail.com>
2022-07-21 09:42:10 +0300

    OSPT-863: Require/Force HTTPS on marketing site

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+/* 
+  This header informs browsers it should only be accessed using HTTPS, instead of using HTTP. 
+  Using the configuration below, all present and future subdomains will use HTTPS for a max-age of 2 years. 
+  This blocks access to pages or subdomains that can only be served over HTTP.
+*/
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  }
+];
+
 const nextConfig = {
-  reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ];
+  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
