ac607c2dfcdd8a08c14dc0f08c76708850db19a0
Erik Sytnyk <erik.sytnyk@gmail.com>
2022-07-22 10:21:34 +0300

    fix heapId

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,16 @@ import Script from 'next/script';
 import env from '@beam-australia/react-env';
 
 function MyApp({Component, pageProps}) {
-  console.log(env('NEXT_PUBLIC_HEAP_ID'));
+  const heapId = process.env.NEXT_PUBLIC_HEAP_ID;
+  console.log(heapId);
 
   return (
     <>
       <Script src="/__ENV.js" strategy="afterInteractive" />
-      {env('NEXT_PUBLIC_HEAP_ID') && (
+      {heapId && (
         <Script id="analytics" strategy="afterInteractive">
           {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-      heap.load(${env('NEXT_PUBLIC_HEAP_ID')});`}
+      heap.load(${heapId});`}
         </Script>
       )}
       <Component {...pageProps} />;
