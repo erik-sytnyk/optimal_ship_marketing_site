fa40de13b384160c29925f089b3e304a8aed3da5
Erik Sytnyk <erik.sytnyk@gmail.com>
2022-07-22 11:52:01 +0300

    add heap analytics and google analytics

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,5 @@
 const baseUrl = 'https://prep.optimalship.com';
 
-let env = 'local';
-console.log('app_env', process.env.APP_ENV);
-console.log('node_env', process.env.NODE_ENV);
-
 const config = {
   baseUrl,
   dhlTrackingNumberUrl: 'https://www.dhl.com/us-en/home/tracking/tracking-express.html',
@@ -20,7 +16,12 @@ const config = {
     facebook: 'https://www.facebook.com/OptimalShipRexburg/',
     instagram: 'https://www.instagram.com/optimalshipteam/',
     linkedIn: 'https://www.linkedin.com/company/optimal-logistics---dallas'
-  }
+  },
+  heapId: {
+    qa: '2466015289',
+    production: '2751061922'
+  },
+  googleAnalytics: 'UA-56168409-2'
 };
 
 export default config;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,58 @@
-import '../styles/index.scss';
 import Script from 'next/script';
+import {useRouter} from 'next/router';
+
+import config from 'src/config';
+
+import '../styles/index.scss';
 
 function MyApp({Component, pageProps}) {
-  const heapId = process.env.NEXT_PUBLIC_HEAP_ID;
-  console.log(heapId);
+  const router = useRouter();
+
+  const env = process.env.NODE_ENV;
+  const isQaEnvironment = router.basePath.includes('optimalship-marketing-qa');
+
+  function getHeapId() {
+    if (isQaEnvironment) return config.heapId.qa;
+
+    if (env === 'production') return config.heapId.production;
+
+    return null;
+  }
+
+  function getGoogleId() {
+    if (isQaEnvironment) return null;
+
+    if (env === 'production') return config.googleAnalytics;
+
+    return null;
+  }
+
+  const heapId = getHeapId();
+  const googleId = getGoogleId();
+
+  console.log('heapId', heapId);
+  console.log('googleId', googleId);
 
   return (
     <>
       <Script src="/__ENV.js" strategy="afterInteractive" />
+      {googleId && (
+        <>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${googleId}`} />
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${googleId}, {
+              page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+          )
+        </>
+      )}
       {heapId && (
         <Script id="analytics" strategy="afterInteractive">
           {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
